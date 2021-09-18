a7ae3fba850c3d0bbb3e84d10d3301b3cc94c312
paulbergstrom <paul.bergs19@gmail.com>
2021-09-18 13:31:53 -0500

    Initial commit

---
+++ forms/ifElse/ifElse.js
@@ -0,0 +1,35 @@
+
+
+//Get user input for first name, state (2 letters), and temprature in Farenheit
+let firstName = prompt ('What is your first name?')
+let userState = prompt ('What state do you loive in (2 letter abbreviation i.e. NE)?')
+let userTemp = Number (prompt('What is the temprature outside in degrees Farenheit?'))
+
+//Store messages
+const messages = [];
+messages[0]= "wear a warm coat, hat, scarf and gloves.";
+messages[1]= "wear a warm coat but you won't need a hat, scarf or gloves.";
+messages[2]= "wear your warmest coat, a warm hat, a scarf, and warm gloves.";
+messages[3]= "wear a warm coat, hat and gloves. Maybe a scarf too."
+
+
+if (userTemp  <32 && userState =="NE") {
+    console.log(messages[0])
+}
+
+else if (userTemp >=32 && userTemp <= 50 && userState == "NE") {
+    console.log(messages[1])
+  }
+  
+else if (userTemp >=32 && userTemp <= 50 && userState == "FL") {
+    console.log(messages[2])
+  }
+  
+  else if (userTemp >=50 && userTemp <= 70 && userState == "FL") {
+    console.log(messages[3])
+  }
+  
+else {
+    console.log("Try again!")
+  }
+    
